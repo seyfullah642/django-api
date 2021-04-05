f846b40438a892a08ce40ba2cb4c4b509cfb4cbb
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-04-05 18:54:29 -0400

    complete test

--- todo/backend/todos/tests.py
+++ todo/backend/todos/tests.py
@@ -13,4 +13,10 @@ class TodoModelTest(TestCase):
     def test_title_content(self):
         todo = Todo.objects.get(id=1)
         expected_object_name = f'{todo.title}'
-        
+        self.assertEquals(expected_object_name, 'first todo')
+
+    
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEquals(expected_object_name, 'a body here')
