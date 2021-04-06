eb89bf94c2504c46920f8e85ef5202d540e600e2
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-04-06 16:49:55 -0400

    created api

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'rest_framework',
+
+    # Local apps
+    'posts.apps.PostsConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
